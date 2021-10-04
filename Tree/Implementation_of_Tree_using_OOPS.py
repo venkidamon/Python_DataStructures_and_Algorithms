ee5f4f00f07effd9c2fb72f99f7e86bfb24defb7
Venkatesh Kumar <venkateshkumartuty@gmail.com>
2021-10-04 12:58:37 +0530

    commit

---
+++ Tree/Implementation_of_Tree_using_OOPS.py
@@ -0,0 +1,73 @@
+class BinaryTree(object):
+    def __init__(self, root_val):
+        self.key = root_val
+        self.left_child = None
+        self.right_child = None
+
+    def insertLeft(self, new_node):
+        if self.left_child == None:
+            self.left_child = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.left_child = self.left_child
+            self.left_child = t
+
+    def insertRight(self, new_node):
+        if self.right_child == None:
+            self.right_child = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.right_child = self.right_child
+            self.right_child = t
+
+    def get_left_child(self):
+        return self.left_child
+
+    def get_right_child(self):
+        return self.right_child
+
+    def get_root_value(self):
+        return self.key
+
+def preorder(tree):
+        
+    if tree:
+        print(tree.get_root_value())
+        preorder(tree.get_left_child())
+        preorder(tree.get_right_child())
+
+def inorder(tree):
+    if tree != None:
+        inorder(tree.get_left_child())
+        print(tree.get_root_value())
+        inorder(tree.get_right_child())
+
+def postorder(tree):
+    if tree != None:
+        postorder(tree.get_left_child())
+        postorder(tree.get_right_child())
+        print(tree.get_root_value())
+    
+
+
+
+
+
+
+
+value = BinaryTree('a')
+
+value.insertLeft('b')
+value.insertRight('c')
+value.insertLeft('d')
+value.insertRight('m')
+value.insertRight('n')
+
+
+preorder(value)
+print("--------------")
+inorder(value)
+print("--------------")
+postorder(value)
+
+
--- Tree/Implementation_of_Tree_using_list.py
+++ Tree/Implementation_of_Tree_using_list.py
@@ -1,1 +1,50 @@
-def 
+def Binary_Tree(root):
+    return [root,[],[]]  #returns a list that contain [root, left part of the root, right part of the root]
+
+def insert_left(root, new_branch):
+
+    t = root.pop(1)                                       #popping out the left side of the root element
+
+    if len(t) > 1:                                       #checking the left side element has more left side 
+        root.insert(1,[new_branch, t,[]])                  #if it had more element we are placing the new_branch and all other below
+    else:
+        root.insert(1,[new_branch,[],[]])               #in case no element at left we insert the new_branch in left
+
+    return root
+
+
+def insert_right(root, new_branch):
+
+    t = root.pop(2)                             #pop(2) because of right element
+
+    if len(t) > 1:
+        root.insert(2,[new_branch, [], t])      #(new_branch, left_side, right_side )
+    else:
+        root.insert(2,[new_branch,[],[]])
+
+    return root
+
+
+def get_root_value(root):
+    return root[0]
+
+def set_root_value(root, new_value):
+    root[0] = new_value
+
+def get_left_child(root):
+    return root[1]
+
+def get_right_child(root):
+    return root[2]
+
+r = Binary_Tree(3)
+
+print(insert_left(r, 4))
+print(insert_left(r, 5))
+print(insert_right(r, 7))   
+print(insert_right(r, 6))
+
+l = get_left_child(r)
+print(l)
+
+
