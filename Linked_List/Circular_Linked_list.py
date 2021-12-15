d186f8e2d951690a7d5b24e516e4339bd0527bea
Venkatesh Kumar <venkateshkumartuty@gmail.com>
2021-12-15 15:10:24 +0530

    commit

--- Linked_List/Circular_Linked_list.py
+++ Linked_List/Circular_Linked_list.py
@@ -27,23 +27,102 @@ class Circular_LinkedList:
             self._tail._next = newest
         self._tail = newest
         self._size += 1
+    
+    def add_first(self, ele):
+        newest = Node(ele)
+        if self.isEmpty():
+            newest._next = newest
+            self._tail = newest
+        else:
+            self._tail._next = newest
+            newest._next = self._head
+        
+        self._head = newest
+        self._size += 1
+
+    def add_any(self, ele, position):
+        newest = Node(ele)
+        p = self._head
+        i = 1
+        while i < position-1:
+            p = p._next
+            i += 1
+        newest._next = p._next
+        p._next = newest
+        self._size += 1
+
+    def remove_first(self):
+        if self.isEmpty():
+            print('list is empty')
+            return
+        e = self._head._element
+        self._tail._next = self._head._next
+        self._head = self._head._next
+        self._size -= 1  
+        if self.isEmpty():
+            self._head = None
+            self._tail = None
+        return e
+
+    def remove_last(self):
+        if self.isEmpty():
+            print('list is empty')
+            return
+        p = self._head
+        i = 1
+        while i < len(self)-1:
+            p = p._next
+            i += 1
+        self._tail = p
+        e = p._next._element
+        self._tail._next = self._head
+        self._size -= 1
+        return e
+
+    
+    def remove_any(self, position):
+        p = self._head
+        i = 1
+        while i < position - 1:
+            p = p._next
+            i += 1
+        e = p._next._element
+        p._next = p._next._next
+        self._size -= 1
+        return e
+        
 
     def display(self):
         p = self._head
         i = 0
         while i < len(self):
-            print(p._element)
+            print(p._element, end='-->')
             p = p._next
             i += 1
-    
+        print()
+
 
 
 
 
 
 l = Circular_LinkedList()
-
+l.add_first(27)
 l.add_last(5)
 l.add_last(6)
 l.add_last(7)
+l.add_first(1)
+l.add_any(7,3)
+l.add_any(1,3)
 l.display()
+l.remove_first()
+r = l.remove_last()
+
+print(r)
+l.display()
+k = l.remove_last()
+l.display()
+
+l.remove_any(2)
+l.display()
+print(len(l))
